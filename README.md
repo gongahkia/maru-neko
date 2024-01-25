e7c64c432b6bcf52eb514449b937fdd6f3f9add3
gongahkia <angryapplegravy@gmail.com>
2024-01-25 17:38:47 +0800

    neat

---
+++ README.md
@@ -0,0 +1,19 @@
+# たまごウェブ
+
+Tamagebu.
+
+## Motivation
+
+## Installation
+
+### CLI
+
+```console
+$ git clone https://github.com/gongahkia/tamagebu
+$ cd tamagebu
+$ make
+```
+
+### GUI
+
+1. 
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// FUA
+    // display
+        // add pngs for sprites
+        // rework README.md installation
+        // rework Makefile
+        // implement rendering of popup browser, see if can retain extension popup when webpage scrolls down/ left and right clicked
+    // logic
+        // continue working on manifest.json
+        // ** use localstorage api to handle local storage within firefox and chrome and safari
+            // refer to main.html to see if existing can be repurposed
+        // add option to delete localstorage data so cache can be emptied
+        // use placeholder sprite for animals first, ask if anyone can help me draw pngs to be used as sprites
+        // use squash and stretch within CSS and HTML to animate sprites nad their movement
+            // repurpose proof-of-concept code for goose if need animals to follow cursor
+                // give sprite a cute left and right bobble similar to fish in chillquarium
+            // find appropriate pet sprites for each variant selection
+        // work out how to store data for each pet, test for restart on computer
+    // test
+        // whether works on firefox chrome opera safari vivaldi
+
+console.log("thanks");
