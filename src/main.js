155f5af21802847a77c8a31d54a50cce289fa666
gongahkia <angryapplegravy@gmail.com>
2024-01-25 17:43:39 +0800

    added

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@
         // ** use localstorage api to handle local storage within firefox and chrome and safari
             // refer to main.html to see if existing can be repurposed
         // add option to delete localstorage data so cache can be emptied
-        // use placeholder sprite for animals first, ask if anyone can help me draw pngs to be used as sprites
+        // use placeholder sprite for animals first, ask nichole or beth if they can help me draw pngs to be used as sprites
         // use squash and stretch within CSS and HTML to animate sprites nad their movement
             // repurpose proof-of-concept code for goose if need animals to follow cursor
                 // give sprite a cute left and right bobble similar to fish in chillquarium
